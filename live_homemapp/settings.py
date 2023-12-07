0e0c1f3c49b0f31bc62596dd6ad9af0a93d10332
Shivi9999 <shiviciss17@gmail.com>
2023-12-07 19:29:55 +0530

    shivi

--- live_homemapp/settings.py
+++ live_homemapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-=$9@dj(!)@p0x!+$r$bcst+q-=np#ltp=uv=$!_^4#)qp#tm#)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
